c277a74183c84394790049c8322b6f769b8e9c8d
Tamish <tamishbajaj360@gmail.com>
2023-08-22 16:13:09 +0530

    few changes

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -10,7 +10,7 @@ const getAllProductsStatic=async(req,res)=>{
 }
 
 const getAllProducts=async(req,res)=>{
-    const {featured,company,name,sort}=req.query; //Jo schema mei hi nahi hai usse filter out karne ke liye
+    const {featured,company,name,sort,fields,numericFilters}=req.query; //Jo schema mei hi nahi hai usse filter out karne ke liye
 
     const queryObject={}
     if(featured){   // Now if anything not in schema comes that will not create mess
@@ -25,17 +25,51 @@ const getAllProducts=async(req,res)=>{
     if(name){
         queryObject.name= {$regex: name, $options:'i'} // Agar pura naam na likhe aur bass kuch part likhe tab bhi search karke dega
     }
+    if(numericFilters){
+        const operatorMap={
+            '>':'$gt',
+            '>=':'$gte',
+            '<':'$lt',
+            '<=':'$lte',
+            '=':'$eq',
+
+        }
+        const regEx=/\b(>|>=|<|<=|=)\b/g
+        let filters=numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`)
+
+        const options=['price','rating']
+        filters=filters.split(',').forEach((item) => {
+            const [field,operator,value]=item.split('-')
+            if(options.includes(fields)){
+                queryObject[field]={[operator]:Number(value)}
+            }
+            
+        });
+    }
     
 
-    let result=Product.find(queryObject)
+    let result=Product.find(queryObject) // Yes we can chain the sort here but there will be a condition when user will not pass sort
 
+    // Thats the reason we are using if else for sorting here
     if(sort){
         const sortList=sort.split(',').join(' ')
         result=result.sort(sortList)
     }
     else{
         result=result.sort('createAt')
+    }   
+
+    if(fields){
+        const fieldList=fields.split(',').join(' ')
+        result=result.select(fieldList)
     }
+
+    const page=Number(req.query.page) || 1
+    const limit=Number(req.query.limit) || 10
+    const skip=(page-1)*limit;
+
+
+    result=result.skip(skip).limit(limit)
     const products=await result
     //const products=await Product.find(req.query) ->All included without seeing schema
     res.status(200).json({products, nbHits:products.length})
